6265499dd31a2d8215884dbd3459e67e6a19f563
mmiemon <mohaiminulislamlpt@gmail.com>
2018-07-26 22:57:18 +0600

    Add files via upload

---
+++ Windows/installer.bat
@@ -0,0 +1,5 @@
+GDCM.exe
+MicroDicom.exe
+This software was developed by Md.Jakaria, Kowshika Sarker, Mostofa Rafid Uddin, Md.Mohaiminul Islam and 
+Trisha Das under the supervision of Md. Shamsuzzoha Bayzid at undergraduate thesis of 2013 batch of CSE 
+department of Bangladesh University of Engineering and Technology.
