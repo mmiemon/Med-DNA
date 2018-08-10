ec21618775238e6880df9f45874b82078275bfb6
mmiemon <mohaiminulislamlpt@gmail.com>
2018-08-10 23:54:11 +0600

    Delete installer.bat

--- Windows/installer.bat
+++
@@ -1,5 +0,0 @@
-GDCM.exe
-MicroDicom.exe
-This software was developed by Md.Jakaria, Kowshika Sarker, Mostofa Rafid Uddin, Md.Mohaiminul Islam and 
-Trisha Das under the supervision of Md. Shamsuzzoha Bayzid at undergraduate thesis of 2013 batch of CSE 
-department of Bangladesh University of Engineering and Technology.
